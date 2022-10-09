4b55238581c16b02768c97e1986d8ef6d5a8bfe8
webashraf <aliashraftamim@gmail.com>
2022-10-09 19:29:48 +0600

    Add Spead Operator Script

--- speadOperator.js
+++ speadOperator.js
@@ -3,6 +3,11 @@ const number2 = number1.map(v => {
     return v+1;
 })
 console.log(number2);
-
-let total = [...number1] + [...number2];
+let total = [...number1, ...number2];
 console.log(total);
+
+// Heigst number using spead Operator
+const theHeigstNumber = [4564, 646844, 64562, 84894];
+
+const hegstResult = Math.max(...theHeigstNumber);
+console.log(hegstResult);
